c75d056201e421690a4678a53d788da9c06a57cb
Pedro26fer <p2698fernandes@gmail.com>
2025-04-13 22:54:00 -0300

    feat: responsividade no sidebar

--- spot-mkt-desk/src/app/components/SideBar.tsx
+++ spot-mkt-desk/src/app/components/SideBar.tsx
@@ -10,7 +10,7 @@ const menu = [
 
 function Sidebar() {
   return (
-    <aside className="group fixed md:relative top-0 left-0 z-50 h-screen w-14 md:w-60 bg-primary text-white transition-all duration-300 ease-in-out hover:w-60 overflow-hidden">
+    <aside className="group fixed md:relative top-0 left-0 z-50 h-screen w-14 md:w-60 bg-primary text-white transition-all duration-300 ease-in-out sm:hidden md:flex flex-col items-center justify-between shadow-lg xs:hidden">
       <div className="p-4 flex flex-col gap-6">
         <h2 className="text-2xl font-bold hidden md:block group-hover:block">SPOT MKT</h2>
         {menu.map((item, i) => (
